e58f17b7ed42c07714c7a6fc3758ae4b49eae88a
SuperCodeAurora <charles.fang.huang@gmail.com>
2026-02-20 10:51:53 +0800

    Update TitaniumCoreApp.swift

--- TitaniumCore.swiftpm/TitaniumCoreApp.swift
+++ TitaniumCore.swiftpm/TitaniumCoreApp.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 @main
 struct TitaniumCoreApp: App {
+    // 🧠 IGNITION: We boot up the Titanium Brain here
+    @StateObject private var tabManager = TabManager()
+
     var body: some Scene {
         WindowGroup {
-            // This tells the app to load the "Shell" (ContentView) we designed earlier.
-            ContentView() 
-                .preferredColorScheme(.dark) // Forces the app into "Dark Mode" for that sleek Titanium vibe.
+            ContentView()
+                // 🔌 INJECTION: This plugs the Brain into the entire app
+                .environmentObject(tabManager) 
+                .preferredColorScheme(.dark)
         }
     }
 }
